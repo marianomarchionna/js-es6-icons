bcf70fc8232eb67ca40286567b5b936576b0da99
Mariano <marianomarchionna8@gmail.com>
2021-09-02 17:36:58 +0200

    Arrow function

--- js/main.js
+++ js/main.js
@@ -103,6 +103,70 @@ const icons = [
       'purple'
   ]
 
+// Sezione Arrow Function
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+const print = (array, container) => {
+  container.innerHTML = ''; 
+  array.forEach((element) => {
+      const { name, prefix, type, family, color } = element;
+      container.innerHTML += `
+      <div class="square">
+          <i class="${family} ${prefix + name}" style="color: ${color}"></i>
+          <div class="title">${name.toUpperCase()}</div>
+      </div>`;
+  });
+};
+
+// Milestone 2
+// Coloriamo le icone per tipo
+const getTypes = (array) => {
+  const types = [];
+  array.forEach((element) => {
+    if(!types.includes(element.type)){
+      types.push(element.type);
+    }
+  });
+  return types;
+};
+
+const colorIcons = (array, colors) => {
+  const types = getTypes(array);
+  const coloredArray = array.map((element) =>{
+    const indexType = types.indexOf(element.type);
+    element.color = colors[indexType];
+    return element;
+  });
+  return coloredArray;
+};
+
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+const printOptions = (array, select) => {
+  array.forEach((element) => {
+    select.innerHTML += 
+    `
+      <option value="${element}">${element}</option>
+    `;
+  });
+};
+
+const filterValues = (array, type) =>{
+  const filteredIcons = array.filter((element) =>{
+    if(element.type === type) {
+      return true;
+    }
+    return false;
+  });
+  if(type === '') {
+    return array;
+  }
+  return filteredIcons;
+};
+
+
+// parte logica
+
 const iconsContainer = document.getElementById('icons');
 
 const coloredArray = colorIcons(icons, colors);
@@ -117,67 +181,3 @@ select.onchange = function(element) {
 
   print(filtered, iconsContainer);
 };
-// funzioni
-
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-
-function print(array, container){
-    container.innerHTML = ''; 
-    array.forEach((element) => {
-        const { name, prefix, type, family, color } = element;
-        container.innerHTML += `
-        <div class="square">
-            <i class="${family} ${prefix + name}" style="color: ${color}"></i>
-            <div class="title">${name.toUpperCase()}</div>
-        </div>`;
-    });
-}
-
-// Milestone 2
-// Coloriamo le icone per tipo
-
-function getTypes(array) {
-  const types = [];
-  array.forEach((element) => {
-    if(!types.includes(element.type)){
-      types.push(element.type);
-    }
-  });
-  return types;
-}
-
-function colorIcons(array, colors){
-  const types = getTypes(array);
-  const coloredArray = array.map((element) =>{
-    const indexType = types.indexOf(element.type);
-    element.color = colors[indexType];
-    return element;
-  });
-  return coloredArray;
-}
-
-// Milestone 3
-// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
-
-function printOptions(array, select){
-  array.forEach((element) => {
-    select.innerHTML += 
-    `
-      <option value="${element}">${element}</option>
-    `;
-  });
-}
-
-function filterValues(array, type) {
-  const filteredIcons = array.filter((element) =>{
-    if(element.type === type) {
-      return true;
-    }
-    return false;
-  });
-  if(type === '') {
-    return array;
-  }
-  return filteredIcons;
-}
