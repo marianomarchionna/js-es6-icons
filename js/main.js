8212828fb3b33050a5851617800843c2183bd825
Mariano <marianomarchionna8@gmail.com>
2021-09-02 16:42:21 +0200

    Milestone2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-// Milestone 2
-// Coloriamo le icone per tipo
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 const icons = [
@@ -108,19 +104,48 @@ const icons = [
       'orange',
       'purple'
   ]
-  
+
 const iconsContainer = document.getElementById('icons');
-print(icons, iconsContainer);
+
+const coloredArray = colorIcons(icons, colors);
+print(coloredArray, iconsContainer);
 
 // funzioni
+
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
 function print(array, container){
     container.innerHTML = ''; 
     array.forEach((element) => {
-        const { name, prefix, type, family } = element;
-        iconsContainer.innerHTML += `
+        const { name, prefix, type, family, color } = element;
+        container.innerHTML += `
         <div class="square">
-            <i class="${family} ${prefix + name}"></i>
+            <i class="${family} ${prefix + name}" style="color: ${color}"></i>
             <div class="title">${name.toUpperCase()}</div>
         </div>`;
     });
+}
+
+// Milestone 2
+// Coloriamo le icone per tipo
+
+function getTypes(array) {
+  const types = [];
+  array.forEach((element) => {
+    if(!types.includes(element.type)){
+      types.push(element.type);
+    }
+  });
+  return types;
+}
+
+function colorIcons(array, colors){
+  const types = getTypes(array);
+  const coloredArray = array.map((element) =>{
+    const indexType = types.indexOf(element.type);
+    element.color = colors[indexType];
+    return element;
+  });
+  return coloredArray;
 }
